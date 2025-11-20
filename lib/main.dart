e9e737ada04608620956d9f02b1b25e7a9cd46ff
Yakuran <rittitarou@yahoo.co.jp>
2025-11-20 12:52:46 +0900

    ✨feat: テンプレートリポジトリの共通機能を追加

--- lib/main.dart
+++ lib/main.dart
@@ -2,9 +2,12 @@ import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:itemized_share_app/app/app.dart';
 import 'package:itemized_share_app/app/core/app_initializer.dart';
+import 'package:itemized_share_app/app/core/shared_preferences/state/shared_preferences_provider.dart';
 import 'package:itemized_share_app/app/core/state/app_build_config_provider.dart';
+import 'package:itemized_share_app/app/routes/state/initial_location_provider.dart';
 import 'package:itemized_share_app/i18n/strings.g.dart';
 import 'package:nested/nested.dart';
+import 'package:shared_preferences/shared_preferences.dart';
 
 void main() async {
   // アプリの初期処理
@@ -13,8 +16,15 @@ void main() async {
   runApp(
     ProviderScope(
       overrides: [
+        initialLocationProvider.overrideWithValue(null),
+
         // 起動時に取得したアプリの基本情報を設定
         appBuildConfigProvider.overrideWithValue(initializedResult.buildConfig),
+
+        // SharedPreferencesの初期化(起動時に同期)
+        sharedPreferencesProvider.overrideWithValue(
+          await SharedPreferences.getInstance(),
+        ),
       ],
       child: Nested(
         children: const [
